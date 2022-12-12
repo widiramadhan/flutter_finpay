c2d84b0c806b25eaf6108f29bca1555ab163f24e
Widi Ramadhan <widiyanto.ramadhan@gmail.com>
2022-12-12 10:04:00 +0700

    first commit

---
+++ android/app/src/main/kotlin/merchant/finpay/flutter/merchant_finpay_flutter/MainActivity.kt
@@ -0,0 +1,38 @@
+package merchant.finpay.flutter.merchant_finpay_flutter
+
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.plugins.util.GeneratedPluginRegister
+import io.flutter.plugin.common.MethodChannel
+import lib.finpay.sdk.corekit.FinpaySDK
+import lib.finpay.sdk.corekit.model.Credential
+import lib.finpay.sdk.uikit.FinpaySDKUI
+
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "lib.finpay.sdk/channels"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegister.registerGeneratedPlugins(FlutterEngine(this@MainActivity))
+        flutterEngine?.dartExecutor?.let {
+            MethodChannel(it, CHANNEL).setMethodCallHandler { call, result ->
+                openApplication()
+            }
+        }
+    }
+
+    private fun openApplication() {
+        FinpaySDKUI.openApplication(this@MainActivity, credential())
+    }
+
+    fun credential(): Credential {
+        val cd = Credential()
+        cd.setUsername("MT77764DKM83N")
+        cd.setPassword("YJV3AM0y")
+        cd.setSecretKey("daYumnMb")
+        cd.setPhoneNumber("083815613839")
+        cd.setCustName("Widiyanto Ramadhan")
+        return cd
+    }
+}
